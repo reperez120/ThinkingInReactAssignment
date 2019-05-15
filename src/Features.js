947a66bcf87cf2ba91010c9ae0fdf34f7e8d3597
reperez120 <46304321+reperez120@users.noreply.github.com>
2019-05-15 19:56:29 -0400

    Update Features.js

--- src/Features.js
+++ src/Features.js
@@ -2,34 +2,40 @@ import React from 'react';
 import './Features.css'
 
 export default class Features extends React.Component {
-  render(){
-      function displayFeatures(features, selected) {
-          return Object.keys(features).map(key => {
-            const options = features[key].map((item, index) => {
-              const selectedClass = item.name === selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  onClick={e => this.props.updateFeature(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-                      .format(item.cost) })
-                </div>
-              </li>
-          });
-          return <div className="feature" key={key}>
+  
+  generateHTML (index, featureClass, item, key) {
+    return (
+      <li key={index} className="feature__item">
+        <div className={featureClass}
+          onClick={e => this.props.updateFeature(key, item)}>
+            { item.name }
+            ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+              .format(item.cost) })
+        </div>
+      </li>
+    )
+  }
+ 
+  displayFeatures() {
+      return Object.keys(this.props.features).map(key => {
+        const options = this.props.features[key].map((item, index) => {
+          const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+          const featureClass = 'feature__option ' + selectedClass;
+          return this.generateHTML(index, featureClass, item, key)
+        });
+        return (
+          <div className="feature" key={key}>
             <div className="feature__name">{key}</div>
             <ul className="feature__list">
               { options }
             </ul>
           </div>
-        });    
-      }  
-  return (
-      <div>
-      { displayFeatures(this.props.features, this.props.selected) }
-      </div>
-  )
+        )
+    });    
+  }  
+
+  render() {
+      return <div>{ this.displayFeatures()}</div>
   }
 }
 
